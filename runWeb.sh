e93018f10a5098db15bec03a2d227abad9dd2562
Fabio Mancinelli <fabio.mancinelli@xwiki.com>
2011-11-08 15:11:39 +0100

    Added logic to set the HSQLDB location (take 2)

--- runWeb.sh
+++ runWeb.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-# Default DB location for the HSQL database
-DEFAULT_DB_LOCATION=$HOME/xwiki_database
+# DB location for the HSQL database. Uncomment to use it
+# DB_LOCATION=$HOME/xwiki_database
 
+# If a parameter is specified on the command line, use it as the DB location
 if [ "$1" != "" ]; then
   DB_LOCATION=$1
-else
-  DB_LOCATION=$DEFAULT_DB_LOCATION
 fi
 
 mvn -o -nsu clean install -Pdev -DskipTests=true -DfailIfNoTests=false
@@ -18,9 +17,11 @@ echo ">>> Unzipping archive"
 unzip wiki30-distribution-zip-1.0-SNAPSHOT.zip
 cd wiki30-distribution-zip-1.0-SNAPSHOT
 
-echo ">>> Setting the DB location"
-mkdir -p $DB_LOCATION
-sed -i "s,jdbc:hsqldb:file:database/xwiki_db,jdbc:hsqldb:file:${DB_LOCATION}/xwiki_db," webapps/xwiki/WEB-INF/hibernate.cfg.xml 
+if [ "$DB_LOCATION" != "" ]; then
+  echo ">>> Setting the DB location"
+  mkdir -p $DB_LOCATION
+  sed -i "s,jdbc:hsqldb:file:database/xwiki_db,jdbc:hsqldb:file:${DB_LOCATION}/xwiki_db," webapps/xwiki/WEB-INF/hibernate.cfg.xml 
+fi
 
 echo ">>> Starting XWiki"
 ./start_xwiki.sh
