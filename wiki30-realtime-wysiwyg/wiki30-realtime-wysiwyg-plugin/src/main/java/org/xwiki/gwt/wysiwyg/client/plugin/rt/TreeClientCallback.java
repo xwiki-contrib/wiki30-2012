670681af908014afee8d78ba7a339059a2883054
Bogdan Flueras <Bogdan.Flueras@loria.fr>
2012-01-20 17:59:35 +0100

    Decouple TreeClientCallback from jupiter core

---
+++ wiki30-realtime-wysiwyg/wiki30-realtime-wysiwyg-plugin/src/main/java/org/xwiki/gwt/wysiwyg/client/plugin/rt/TreeClientCallback.java
@@ -0,0 +1,98 @@
+package org.xwiki.gwt.wysiwyg.client.plugin.rt;
+
+import com.google.gwt.dom.client.Node;
+import com.google.gwt.dom.client.Text;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Element;
+import fr.loria.score.client.ClientCallback;
+import fr.loria.score.client.ClientDTO;
+import fr.loria.score.client.Converter;
+import fr.loria.score.jupiter.model.Document;
+import fr.loria.score.jupiter.model.Message;
+import fr.loria.score.jupiter.tree.TreeDocument;
+import fr.loria.score.jupiter.tree.operation.*;
+
+/**
+ * Callback for tree documents, wysiwyg editor
+ */
+public class TreeClientCallback implements ClientCallback {
+    private Node nativeNode;
+
+    public TreeClientCallback(Node nativeNode) {
+        this.nativeNode = nativeNode;
+    }
+
+    @Override
+    public void onConnected(ClientDTO dto, Document document, boolean updateUI) {
+        if (updateUI) {
+            log.finest("Updating UI for WYSIWYG. Replacing native node: " + Element.as(nativeNode).getString());
+            Node newNode = Converter.fromCustomToNative(((TreeDocument) document).getRoot());
+            nativeNode.getParentNode().replaceChild(newNode, nativeNode);
+            nativeNode = newNode;
+            log.finest("New native node: " + Element.as(nativeNode).getString());
+        }
+    }
+
+    @Override
+    public void onDisconnected() {
+    }
+
+    @Override
+    public void onExecute(Message receivedMessage) {   // todo: generify
+        log.fine("Executing received: " + receivedMessage);
+        log.fine("Native node is before: " + Element.as(nativeNode).getString());
+        TreeOperation operation = (TreeOperation) receivedMessage.getOperation();
+        int position = operation.getPosition();
+
+        final Node targetNode = TreeHelper.getChildNodeFromLocator(nativeNode, operation.getPath());
+
+        if (operation instanceof TreeInsertText) {
+            //operates on a text node
+            TreeInsertText insertText = (TreeInsertText) operation;
+            String txt = String.valueOf(insertText.getText());
+
+            if (targetNode == nativeNode) {
+                Node textNode = com.google.gwt.dom.client.Document.get().createTextNode(txt);
+                //some browsers insert a br element on an empty text area, so remove it
+                Node brElement = nativeNode.getChild(0);
+                if (brElement != null) {
+                    nativeNode.replaceChild(textNode, brElement);
+                } else {
+                    nativeNode.appendChild(textNode);
+                }
+            } else {
+                targetNode.setNodeValue(targetNode.getNodeValue() + txt);
+            }
+        } else if (operation instanceof TreeDeleteText) {
+            TreeDeleteText deleteText = (TreeDeleteText) operation;
+
+            Text textNode = (Text) targetNode;
+            textNode.deleteData(position, 1); //delete 1 char
+        } else if (operation instanceof TreeInsertParagraph) {
+            TreeInsertParagraph treeInsertParagraph = (TreeInsertParagraph) operation;
+
+            //Get the actual text node
+            //first remove from the textnode what was after caret position
+            String actualText = targetNode.getNodeValue();
+            targetNode.setNodeValue(actualText.substring(0, position));
+
+            //then insert new node
+            Node n = targetNode.getParentElement();
+            log.fine("Parent node: " + Element.as(n).getString());
+            Element p = DOM.createElement("p");
+            p.setInnerText(actualText.substring(position));
+            n.getParentElement().insertAfter(p, n);
+        } else if (operation instanceof TreeMergeParagraph) {
+            Node p = targetNode.getParentElement();
+            Node pPreviousSibling = p.getPreviousSibling();
+
+            targetNode.removeFromParent();
+            p.removeFromParent();
+
+            Node oldTextNode = pPreviousSibling.getChild(0);
+            Text newTextNode = com.google.gwt.dom.client.Document.get().createTextNode(oldTextNode.getNodeValue() + targetNode.getNodeValue());
+            pPreviousSibling.replaceChild(newTextNode, oldTextNode);
+        }
+        log.fine("Native node is after: " + Element.as(nativeNode).getString());
+    }
+}
--- wiki30-realtime/rt-gwt-client/src/main/java/fr/loria/score/client/ClientCallback.java
+++ wiki30-realtime/rt-gwt-client/src/main/java/fr/loria/score/client/ClientCallback.java
@@ -1,18 +1,8 @@
 package fr.loria.score.client;
 
-import com.google.gwt.dom.client.Node;
-import com.google.gwt.dom.client.Text;
-import com.google.gwt.user.client.DOM;
-import com.google.gwt.user.client.Element;
 import fr.loria.score.jupiter.model.Document;
 import fr.loria.score.jupiter.model.Message;
 import fr.loria.score.jupiter.plain.operation.Operation;
-import fr.loria.score.jupiter.tree.TreeDocument;
-import fr.loria.score.jupiter.tree.TreeUtils;
-import fr.loria.score.jupiter.tree.operation.*;
-import org.xwiki.gwt.dom.mutation.client.DefaultMutationOperator;
-import org.xwiki.gwt.dom.mutation.client.Mutation;
-import org.xwiki.gwt.dom.mutation.client.MutationOperator;
 
 import java.util.logging.Logger;
 
@@ -86,90 +76,6 @@ public interface ClientCallback {
             }
 
             editor.paint(); //cursor.focus() paints it, but it might occur that the page is not focused and thus not updated
-        }
-    }
-
-    /**
-     * Callback for tree documents, wysiwyg editor
-     */
-    public class TreeClientCallback implements ClientCallback {
-        private Node nativeNode;
-
-        public TreeClientCallback(Node nativeNode) {
-            this.nativeNode = nativeNode;
-        }
-
-        @Override
-        public void onConnected(ClientDTO dto, Document document, boolean updateUI) {
-            if (updateUI) {
-                log.finest("Updating UI for WYSIWYG. Replacing native node: " + Element.as(nativeNode).getString());
-                Node newNode = Converter.fromCustomToNative(((TreeDocument)document).getRoot());
-                nativeNode.getParentNode().replaceChild(newNode, nativeNode);
-                nativeNode = newNode;
-                log.finest("New native node: " + Element.as(nativeNode).getString());
-            }
-        }
-
-        @Override
-        public void onDisconnected() {}
-
-        @Override
-        public void onExecute(Message receivedMessage) {   // todo: generify
-            log.fine("Executing received: " + receivedMessage);
-            log.fine("Native node is before: " + Element.as(nativeNode).getString());
-            TreeOperation operation = (TreeOperation) receivedMessage.getOperation();
-            int position = operation.getPosition();
-
-            Mutation mutation = new Mutation();
-            final String locator = TreeUtils.getStringLocatorFromPath(operation.getPath());
-            mutation.setLocator(locator);
-
-            MutationOperator operator = new DefaultMutationOperator();
-
-            if (operation instanceof TreeInsertText) {
-                //operates on a text node
-                TreeInsertText tit = (TreeInsertText) operation;
-
-                mutation.setType(Mutation.MutationType.INSERT);
-                mutation.setValue(String.valueOf(position) + "," + tit.getText()); // insert 1 char
-
-                operator.operate(mutation, nativeNode);
-            } else if (operation instanceof TreeDeleteText) {
-                TreeDeleteText treeDeleteText = (TreeDeleteText) operation;
-
-                mutation.setType(Mutation.MutationType.REMOVE);
-                mutation.setValue(String.valueOf(position) + "," + String.valueOf(position + 1)); // delete 1 char
-
-                operator.operate(mutation, nativeNode);
-            } else if (operation instanceof TreeInsertParagraph) {
-                TreeInsertParagraph treeInsertParagraph = (TreeInsertParagraph) operation;
-
-                //Get the actual text node
-                //first remove from the textnode what was after caret position
-                Node textNode = DefaultMutationOperator.getChildNodeFromLocator(nativeNode, locator);
-                String actualText = textNode.getNodeValue();
-                textNode.setNodeValue(actualText.substring(0, position));
-
-                //then insert new node
-                Node n = textNode.getParentElement();
-                log.fine("Parent node: " + Element.as(n).getString());
-                Element p = DOM.createElement("p");
-                p.setInnerText(actualText.substring(position));
-                n.getParentElement().insertAfter(p, n);
-            } else if (operation instanceof TreeMergeParagraph) {
-                Node textNode = DefaultMutationOperator.getChildNodeFromLocator(nativeNode, locator);
-                Node p = textNode.getParentElement();
-                Node pPreviousSibling = p.getPreviousSibling();
-
-                textNode.removeFromParent();
-                p.removeFromParent();
-
-                Node oldTextNode = pPreviousSibling.getChild(0);
-                Text newTextNode = com.google.gwt.dom.client.Document.get().createTextNode(oldTextNode.getNodeValue() + textNode.getNodeValue());
-                pPreviousSibling.replaceChild(newTextNode, oldTextNode);
-            }
-            log.fine("Applied mutation: " + mutation);
-            log.fine("Native node is after: " + Element.as(nativeNode).getString());
         }
     }
 }
