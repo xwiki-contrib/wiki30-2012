a3e0b453d1d1d1d02ee17290581807aa9bee578c
Bogdan Flueras <Bogdan.Flueras@loria.fr>
2012-02-02 14:34:39 +0100

    Implement super class method

--- wiki30-realtime-wysiwyg/wiki30-realtime-wysiwyg-plugin/src/main/java/org/xwiki/gwt/wysiwyg/client/plugin/rt/TreeClientCallback.java
+++ wiki30-realtime-wysiwyg/wiki30-realtime-wysiwyg-plugin/src/main/java/org/xwiki/gwt/wysiwyg/client/plugin/rt/TreeClientCallback.java
@@ -7,9 +7,13 @@ import com.google.gwt.dom.client.NodeList;
 import fr.loria.score.client.ClientCallback;
 import fr.loria.score.client.ClientDTO;
 import fr.loria.score.client.Converter;
+import fr.loria.score.jupiter.model.AbstractOperation;
 import fr.loria.score.jupiter.model.Message;
 import fr.loria.score.jupiter.tree.Tree;
 import fr.loria.score.jupiter.tree.TreeDocument;
+import fr.loria.score.jupiter.tree.operation.TreeDeleteText;
+import fr.loria.score.jupiter.tree.operation.TreeInsertText;
+import fr.loria.score.jupiter.tree.operation.TreeOperation;
 
 /**
  * Callback for tree documents, wysiwyg editor
@@ -36,10 +40,36 @@ public class TreeClientCallback implements ClientCallback {
     }
 
     @Override
-    public void onExecute(Message receivedMessage) {
-        log.info("Root is before: " + root);
+    public void beforeSend(Message message) {
+        AbstractOperation op = message.getOperation();
+        if (op instanceof TreeOperation) {
+            log.finest("Before send");
+            TreeOperation treeOperation = (TreeOperation) op;
+            int index = treeOperation.getPath()[0];
+            boolean update = true;
+
+            if (op instanceof TreeInsertText || op instanceof TreeDeleteText) {
+                update = false;
+            }
+
+            //modify DOM
+            if (update) {
+                //todo: be smarter and update just some paragraph NOT all DOM
+//                Tree treeParagraph = root.getChild(index);
+//                Node nodeParagraph = nativeNode.getChild(index);
+//                log.info("New node is:" + Element.as(replaceDOMNode(treeParagraph, nodeParagraph)).getInnerHTML());
+
+                  updateDOM();
+                //todo:  set the selection
+            }
+        }
+    }
+
+    @Override
+    public void afterReceive(Message receivedMessage) {
+        log.finest("Root is before: " + root);
         updateDOM();
-        log.fine("Root is after: " + root);
+        log.finest("Root is after: " + root);
 
     }
 
@@ -72,10 +102,20 @@ public class TreeClientCallback implements ClientCallback {
 
     private void updateDOM() {
         log.fine("Native node is before: " + Element.as(nativeNode).getString());
-        Node newNode = Converter.fromCustomToNative(root);
-        insertBrInEmptyParagraphs(newNode);
-        nativeNode.getParentNode().replaceChild(newNode, nativeNode);
-        nativeNode = newNode;
+        nativeNode = replaceDOMNode(root, nativeNode);
         log.fine("Native node is after: " + Element.as(nativeNode).getString());
+    }
+
+    /**
+     * Updates and replaces the DOM node (structurally) according to the tree node
+     * @param tree the model upon to update the DOM node
+     * @param node to be replaced
+     * @return the replaced DOM node reflecting the structure of the tree
+     */
+    private Node replaceDOMNode(Tree tree, Node node) {
+        Node newNode = Converter.fromCustomToNative(tree);
+        insertBrInEmptyParagraphs(newNode);
+        node.getParentNode().replaceChild(newNode, node);
+        return newNode;
     }
 }
