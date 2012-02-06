73702bf8c236615384b9fdcdd6e3d505b40c983a
Bogdan Flueras <Bogdan.Flueras@loria.fr>
2012-02-06 10:44:03 +0100

    DOM rendering optimisation on receivers: insert and delete text

--- wiki30-realtime-wysiwyg/wiki30-realtime-wysiwyg-plugin/src/main/java/org/xwiki/gwt/wysiwyg/client/plugin/rt/TreeClientCallback.java
+++ wiki30-realtime-wysiwyg/wiki30-realtime-wysiwyg-plugin/src/main/java/org/xwiki/gwt/wysiwyg/client/plugin/rt/TreeClientCallback.java
@@ -1,9 +1,6 @@
 package org.xwiki.gwt.wysiwyg.client.plugin.rt;
 
-import com.google.gwt.dom.client.Document;
-import com.google.gwt.dom.client.Element;
-import com.google.gwt.dom.client.Node;
-import com.google.gwt.dom.client.NodeList;
+import com.google.gwt.dom.client.*;
 import fr.loria.score.client.ClientCallback;
 import fr.loria.score.client.ClientDTO;
 import fr.loria.score.client.Converter;
@@ -94,10 +91,112 @@ public class TreeClientCallback implements ClientCallback {
 
     @Override
     public void afterReceive(Message receivedMessage) {
-        log.finest("Root is before: " + customNode);
-        updateDOM();
-        log.finest("Root is after: " + customNode);
+        log.fine("Executing received: " + receivedMessage);
+        TreeOperation operation = (TreeOperation) receivedMessage.getOperation();
 
+        final int position = operation.getPosition();
+        final int[] path = operation.getPath();
+        final Node targetNode = TreeHelper.getChildNodeFromLocator(nativeNode, path);
+        final short targetNodeType = targetNode.getNodeType();
+
+        if (operation instanceof TreeInsertText) {
+            //operates on a text node
+            TreeInsertText insertText = (TreeInsertText) operation;
+            String txt = String.valueOf(insertText.getText());
+
+            if (Node.ELEMENT_NODE == targetNodeType) {
+                Node newTextNode = Document.get().createTextNode(txt);
+                Node node = targetNode.getChild(position);
+                targetNode.insertBefore(newTextNode, node);
+            } else if (Node.TEXT_NODE == targetNodeType) {
+                Text.as(targetNode).insertData(position, txt);
+            }
+        }
+        else if (operation instanceof TreeDeleteText) {
+            TreeDeleteText deleteText = (TreeDeleteText) operation;
+            if (Node.TEXT_NODE == targetNodeType) {
+                Text textNode = Text.as(targetNode);
+                textNode.deleteData(position, 1); //delete 1 char
+            } else if (Node.ELEMENT_NODE == targetNodeType) {
+                // there is no delete text op generated on elements yet
+            }
+        }
+//        } else if (operation instanceof TreeInsertParagraph) {
+//            TreeInsertParagraph treeInsertParagraph = (TreeInsertParagraph) operation;
+//            // cases
+//            //1 hit enter on empty text area
+//            final Element p = Document.get().createElement("p");
+//            if (nativeNode.getChildCount() == 0) {
+//                targetNode.insertFirst(p);
+//            } else if (nativeNode.getChildCount() == 1 && nativeNode.getFirstChild().getNodeName().equalsIgnoreCase("br")) {
+//                log.info("3");
+//                nativeNode.replaceChild(nativeNode.getFirstChild(), p);
+//                //2 hit enter on first line
+//            } else if (position == 0) {
+//                //2.1 enter at the start of the text
+//                // pull down all lines below
+//                p.appendChild(Document.get().createBRElement());
+//                Node parentNode = targetNode.getParentElement();
+//                if (path.length == 1 && path[0] == 0) {
+//                    parentNode.insertBefore(p, targetNode);
+//                } else {
+//                    parentNode.getParentNode().insertBefore(p, parentNode);
+//                }
+//            } else {
+//                log.info("5");
+//                //split the line, assume the targetNode is text
+//                String actualText = targetNode.getNodeValue();
+//                //2.2 enter in the middle of the text
+//                //2.3 enter at the end of the text
+//                //position < actualText.length()
+//                Text textNode = Text.as(targetNode);
+//                textNode.deleteData(0, position);
+//
+//                p.setInnerText(actualText.substring(0, position));
+//
+//                Node parentElement = targetNode.getParentElement();
+//                parentElement.getParentElement().insertBefore(p, parentElement);
+//            }
+//            //3 hit enter in between , not first line
+//            //4 hit enter at the end , nfl
+//
+//            //Get the actual text node
+//            //first remove from the textnode what was after caret position
+//        } else if (operation instanceof TreeNewParagraph) {
+//            TreeNewParagraph treeNewParagraph = (TreeNewParagraph) operation;
+//            //assume position == 0
+//            final Element p = Document.get().createElement("p");
+//            p.appendChild(Document.get().createBRElement());
+//
+//            Node parentNode = targetNode.getParentElement();
+//            if (path.length == 1 && path[0] == 0) {
+//                parentNode.insertBefore(p, targetNode);
+//            } else {
+//                parentNode.getParentNode().insertBefore(p, parentNode);
+//            }
+//        } else if (operation instanceof TreeMergeParagraph) {
+//            Node p = targetNode.getParentElement();
+//            Node pPreviousSibling = p.getPreviousSibling();
+//
+//            targetNode.removeFromParent();
+//            p.removeFromParent();
+//
+//            Node oldTextNode = pPreviousSibling.getLastChild();
+//            Text newTextNode = Document.get().createTextNode(oldTextNode.getNodeValue() + targetNode.getNodeValue());
+//            pPreviousSibling.replaceChild(newTextNode, oldTextNode);
+//        } else if (operation instanceof TreeStyle) {
+//            TreeStyle style = (TreeStyle) operation;
+//            Node parentElement = targetNode.getParentElement();
+//            Element styleElement = DOM.createElement(style.param);
+//            styleElement.appendChild(targetNode);
+//            parentElement.replaceChild(styleElement, targetNode);
+//        }
+        else {
+            log.warning("Update all DOM");
+            log.finest("Root is before: " + customNode);
+            updateDOM();
+            log.finest("Root is after: " + customNode);
+        }
     }
 
     /**
