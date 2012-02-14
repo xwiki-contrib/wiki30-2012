d00d46583c97adffc570304545d9073ed4e2ac57
Bogdan Flueras <Bogdan.Flueras@loria.fr>
2012-02-14 13:38:01 +0100

    Add tests

--- wiki30-realtime/rt-gwt-client/src/test/java/fr/loria/score/TreeOperationsTest.java
+++ wiki30-realtime/rt-gwt-client/src/test/java/fr/loria/score/TreeOperationsTest.java
@@ -1,11 +1,14 @@
 package fr.loria.score;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import fr.loria.score.jupiter.tree.Tree;
 import fr.loria.score.jupiter.tree.TreeFactory;
 import fr.loria.score.jupiter.tree.operation.TreeInsertParagraph;
 import fr.loria.score.jupiter.tree.operation.TreeStyle;
+
+import static org.junit.Assert.fail;
 
 /**
  * Test the effect of executing tree operations on the tree model
@@ -14,31 +17,54 @@ import fr.loria.score.jupiter.tree.operation.TreeStyle;
  */
 public class TreeOperationsTest
 {
+    private Tree root;
+
+    @Before
+    public void init()
+    {
+        root = TreeFactory.createEmptyTree();
+    }
+
     @Test
     public void executeStyle()
     {
-        Tree t = TreeFactory.createEmptyTree();
-
         final Tree paragraphTree = TreeFactory.createParagraphTree();
         paragraphTree.addChild(TreeFactory.createTextTree("abcd"));
-        t.addChild(paragraphTree);
+        root.addChild(paragraphTree);
 
         TreeStyle bold = new TreeStyle(0, new int[] {0, 0}, 0, 4, "bold", "true", true, false, false);
-        bold.execute(t);
+        bold.execute(root);
 
         TreeStyle style1 = new TreeStyle(0, new int[] {0, 0, 0}, 0, 2, "bold", "true", false, false, true);
-        style1.execute(t);
-
-        TreeInsertParagraph p = new TreeInsertParagraph(0, 1, new int[] {0, 0});
-        p.execute(t);
+        style1.execute(root);
 
         //TreeStyle style3 = new TreeStyle(0, new int[] {0, 0, 0}, 0, 2, "style", "bold", false, false, false);
         //style3.execute(t);
 
-
-        // to fix
+        // todo fix
         //TreeStyle style2 = new TreeStyle(0, new int[] {0, 0, 0}, 2, 4, "style", "bold", true, true, false);
         //style2.execute(t);
-        System.out.println("t = " + t);
+        //todo: define equals on trees
+        System.out.println("t = " + root);
+        fail("Fix style");
+    }
+
+    @Test
+    public void executeInsertParagraph()
+    {
+        final Tree paragraphTree = TreeFactory.createParagraphTree();
+        paragraphTree.addChild(TreeFactory.createTextTree("abcd"));
+        root.addChild(paragraphTree);
+
+        TreeStyle bold = new TreeStyle(0, new int[] {0, 0}, 0, 4, "bold", "true", true, false, false);
+        bold.execute(root);
+
+        TreeStyle style1 = new TreeStyle(0, new int[] {0, 0, 0}, 0, 2, "bold", "true", false, false, true);
+        style1.execute(root);
+
+        TreeInsertParagraph p = new TreeInsertParagraph(0, 1, new int[] {0, 0});
+        p.execute(root);
+
+        fail("Fix the problem in InsertParagraph");
     }
 }
