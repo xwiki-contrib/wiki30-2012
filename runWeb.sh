5223ede71300147a07ebe7c99f7571b91c5d205c
Bogdan Flueras <Bogdan.Flueras@loria.fr>
2012-02-17 13:04:51 +0100

    Fix broken build: maven fetches artifacts from server while it should take them from this project

--- runWeb.sh
+++ runWeb.sh
@@ -8,6 +8,12 @@ if [ "$1" != "" ]; then
   DB_LOCATION=$1
 fi
 
+#Temporary hack until we fix the xwiki-platform-gwt-user dependency right
+dir=`pwd`
+cd xwiki-platform-wiki30/xwiki-platform-core/xwiki-platform-gwt/xwiki-platform-gwt-user
+mvn -nsu clean install -DskipTests=true -DfailIfNoTests=false
+cd $dir
+
 #To effectively work with no snapshot updates, please update to maven 3.0.4
 mvn -nsu clean install -Pdev -DskipTests=true -DfailIfNoTests=false
 
