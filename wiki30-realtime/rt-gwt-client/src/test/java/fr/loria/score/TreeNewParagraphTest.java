d05ba1850c3b1680057f216965e9aacc73459380
Gerald Oster <oster@loria.fr>
2012-02-29 22:13:26 +0100

    clean tests on NewParagraph operation

--- wiki30-realtime/rt-gwt-client/src/test/java/fr/loria/score/TreeNewParagraphTest.java
+++ wiki30-realtime/rt-gwt-client/src/test/java/fr/loria/score/TreeNewParagraphTest.java
@@ -11,9 +11,12 @@ import static fr.loria.score.TreeDSL.text;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Tests the TreeInsertParagraph operation in different scenarios
+ * Tests TreeInsertParagraph operation in different cases
  *
  * @author Bogdan.Flueras@inria.fr
+ * @author Luc.Andre@loria.fr
+ * @author Gerald.Oster@loria.fr
+ * 
  */
 public class TreeNewParagraphTest extends AbstractTreeOperationTest
 {
@@ -22,13 +25,29 @@ public class TreeNewParagraphTest extends AbstractTreeOperationTest
     {
         rootDSL.addChild(paragraph().addChild(text("ab")));
 
-        // Split it at the beginning of line
+        // Split it at the start of the line
         final TreeNewParagraph paragraphAtStart = new TreeNewParagraph(SITE_ID, 0);
         paragraphAtStart.execute(rootDSL.getTree());
 
         // expectedRoot = <p>[]</p><p>[ab]</p>
         expectedRootDSL.addChild(paragraph().addChild(text("")),
-            paragraph().addChild(text("ab")));
+                                 paragraph().addChild(text("ab")));
+
+        assertEquals("Invalid tree ", expectedRootDSL.getTree(), rootDSL.getTree());
+    }
+    
+    @Test
+    public void newParagraphAtEndOfLine()
+    {
+        rootDSL.addChild(paragraph().addChild(text("ab")));
+
+        // Split it at the end of the line
+        final TreeNewParagraph paragraphAtStart = new TreeNewParagraph(SITE_ID, 1);
+        paragraphAtStart.execute(rootDSL.getTree());
+
+        // expectedRootDSL.getTree() = <p>[ab]</p><p>[]</p>
+        expectedRootDSL.addChild(paragraph().addChild(text("ab")),
+                                 paragraph().addChild(text("")));
 
         assertEquals("Invalid tree ", expectedRootDSL.getTree(), rootDSL.getTree());
     }
@@ -36,42 +55,53 @@ public class TreeNewParagraphTest extends AbstractTreeOperationTest
     @Test
     public void newParagraphAtStartOfStyledLine()
     {
-        rootDSL.addChild(paragraph().addChild(text("a"), span("style", "bold").addChild(text("b"))));
+        rootDSL.addChild(paragraph().addChild(text("a"), 
+                                              span("style", "bold").addChild(text("b"))));
+ 
         TreeOperation insert = new TreeNewParagraph(SITE_ID, 0);
         insert.execute(rootDSL.getTree());
 
         //expectedRootDSL.getTree() = <p>[]</p><p>[a]<span bold>b</span></p>
-        expectedRootDSL.addChild(paragraph().addChild(text(""))).
-            addChild(paragraph().addChild(text("a"), span("style", "bold").addChild(text("b"))));
+        expectedRootDSL.addChild(paragraph().addChild(text("")),
+                                 paragraph().addChild(text("a"), 
+                                                      span("style", "bold").addChild(text("b"))));
+        
         assertEquals("Invalid tree", expectedRootDSL.getTree(), rootDSL.getTree());
     }
 
     @Test
-    public void newParagraphEndOfLine()
+    public void newParagraphAtEndOfStyledLine()
     {
-        rootDSL.addChild(paragraph().addChild(text("ab")));
+        rootDSL.addChild(paragraph().addChild(text("a"), 
+                                              span("style", "bold").addChild(text("b"))));
+ 
+        TreeOperation insert = new TreeNewParagraph(SITE_ID, 1);
+        insert.execute(rootDSL.getTree());
 
-        // Split it at the beginning of line
-        final TreeNewParagraph paragraphAtStart = new TreeNewParagraph(SITE_ID, 1);
-        paragraphAtStart.execute(rootDSL.getTree());
-
-        // expectedRootDSL.getTree() = <p>[ab]</p><p>[]</p>
-        expectedRootDSL.addChild(paragraph().addChild(text("ab")),
-            paragraph().addChild(text("")));
-
-        assertEquals("Invalid tree ", expectedRootDSL.getTree(), rootDSL.getTree());
+        //expectedRootDSL.getTree() = <p>[a]<span bold>b</span></p><p>[]</p>
+        expectedRootDSL.addChild(paragraph().addChild(text("a"), 
+                                                      span("style", "bold").addChild(text("b"))),
+                                 paragraph().addChild(text("")));
+        
+        assertEquals("Invalid tree", expectedRootDSL.getTree(), rootDSL.getTree());
     }
+
 
     @Test
     public void newParagraphInSpan()
     {
+        // FIXME: I've the feeling that this test is useless since it has the same expectations
+        // than the newParagraphAtStartOfStyledLine test. I think we should remove it.
+        
         rootDSL.addChild(paragraph().addChild(span("style", "bold").addChild(text("b"))));
+        
         TreeOperation insert = new TreeNewParagraph(SITE_ID, 0);
         insert.execute(rootDSL.getTree());
 
         //expectedRootDSL.getTree() = <p>[]</p><p><span bold>[b]</span></p>
-        expectedRootDSL.addChild(paragraph().addChild(text(""))).
-            addChild(paragraph().addChild(span("style", "bold").addChild(text("b"))));
+        expectedRootDSL.addChild(paragraph().addChild(text("")),
+                                 paragraph().addChild(span("style", "bold").addChild(text("b"))));
+        
         assertEquals("Invalid tree", expectedRootDSL.getTree(), rootDSL.getTree());
     }
 }
